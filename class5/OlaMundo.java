50cfee63e280b43d49610b890218b245c2997952
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2020-08-25 14:38:50 -0300

    Change in the name of directory and empyting the directory

--- class5/OlaMundo.java
+++
@@ -1,6 +0,0 @@
-class OlaMundo {
-
-  public static void main(String[] args) {
-    System.out.println("Olá Mundo!");
-  }
-}  
--- class6/Arguments.java
+++
@@ -1,6 +0,0 @@
-class Arguments {
-
-  public static void main(String[] args) {
-    System.out.println("You typed: " + args[0]);
-  }
-}  
